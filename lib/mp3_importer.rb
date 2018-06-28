0006ccd608dd276f8420179495c7cf98521139a9
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 20:45:51 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
   def import
     @filenames = Dir["#{@path}/*.mp3"].map! {|file| file[@path.length+1..-1]}
     @filenames.each {|file_name| Song.new_by_filename(file_name)}
-    Artist.all
+    puts Artist.all
   end
   def files
     import
