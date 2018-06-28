c1077fc93dea16ad37a2cac8da174726dc836503
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 20:54:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,8 @@ class Song
   end
 
   def artist_name=(name)
-    new_song = Artist.find_or_create_by_name(name)
-    Artist.add_song(new_song)
+    self.artist = Artist.find_or_create_by_name(name)
+    Artist.add_song(self)
   end
 end
 
