5dcbd1a99ef655de4ee7c2e6e5ef49687b826cac
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 19:36:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,14 @@ class Artist
 
   @@all = []
 
-  def self.find_or_create_by_name(name)
+  def self.all
+    @@all
+  end
 
+  def find_by_name(name)
+    self.all
+
+  def self.find_or_create_by_name(name)
+    @@all.include?()
 
 end
