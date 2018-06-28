386071d75d911ac25f92344eca65b657742a74a9
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 19:30:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
-  attr_accessor :title, :artist, :genre
+  attr_accessor :title, :artist_name, :genre
 
   def self.new_by_filename(file_name)
     attributes = file_name.split(" - ")
     new_song = Song.new
-    new_song.artist = attributes[0]
+    new_song.artist_name = attributes[0]
     new_song.title = attributes[1]
     new_song.genre = attributes[2]
     new_song
   end
+
+  def artist_name=(artist_name)
 
 end
