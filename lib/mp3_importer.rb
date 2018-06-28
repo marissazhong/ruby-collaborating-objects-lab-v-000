706304dbaca7a217b91ceecee3aec651bfb6e3c1
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 20:46:48 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,5 +17,31 @@ class MP3Importer
 
 end
 
+# for testing
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  def self.all
+    @@all
+  end
+  def initialize(name)
+    @name = name
+  end
+  def self.new_by_filename(file_name)
+    attributes = file_name[0..-5].split(" - ")
+    new_song = Song.new(attributes[1])
+    new_song.artist = attributes[0]
+    new_song.genre = attributes[2]
+    self.all << new_song
+    new_song
+  end
+
+  def artist_name=(name)
+    new_song = Artist.find_or_create_by_name(name)
+    Artist.add_song(new_song)
+  end
+end
+
 dir = MP3Importer.new('./db/mp3s').import
 #puts dir
